c20284e7693ac9ae3d8f2ec761ca4688663494df
andiairlines <aws@sap-architect.com>
2023-07-22 08:39:10 +0200

    v4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import logo from './'andi.jpg;
+import logo from './andi.jpg';
 import './App.css';
 
 function App() {
-	  return (
-		      <div className="App">
-		        <header className="App-header">
-		          <img src={logo} className="App-logo" alt="logo" />
-		          <h1>Hello from V2</h1>
-		        </header>
-		      </div>
-		    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          hello World!
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/andreasmoser3/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit me!
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
